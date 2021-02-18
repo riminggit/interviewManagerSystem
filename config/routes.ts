b89050900bf2ec648e3bb7d67b37d1c3ffd64b67
huang_feixiang <huangfeixiang@loongcheer.com>
2021-02-18 18:39:58 +0800

    初始化项目

---
+++ config/routes.ts
@@ -0,0 +1,135 @@
+﻿export default [
+  {
+    path: '/',
+    component: '../layouts/BlankLayout',
+    routes: [
+      {
+        path: '/user',
+        component: '../layouts/UserLayout',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './User/login',
+          },
+        ],
+      },
+      {
+        path: '/',
+        component: '../layouts/SecurityLayout',
+        routes: [
+          {
+            path: '/',
+            component: '../layouts/BasicLayout',
+            authority: ['admin', 'user'],
+            routes: [
+              {
+                path: '/',
+                redirect: '/userAnalyse',
+              },
+              {
+                path: '/userAnalyse',
+                name: 'userAnalyse',
+                icon: 'AreaChartOutlined',
+                component: './UserAnalyse',
+              },
+              // {
+              //   path: '/topicClassify',
+              //   name: 'topicClassify',
+              //   icon: 'smile',
+              //   component: './UserAnalyse',
+              // },
+              {
+                path: '/topic',
+                name: 'topic',
+                icon: 'ClusterOutlined',
+                component: './Admin',
+                authority: ['admin'],
+                routes: [
+                  {
+                    path: '/admin/add_topic',
+                    name: 'add_topic',
+                    icon: 'smile',
+                    component: './Welcome',
+                    authority: ['admin'],
+                  },
+                  {
+                    path: '/admin/query_topic',
+                    name: 'query_topic',
+                    icon: 'smile',
+                    component: './Welcome',
+                    authority: ['admin'],
+                  },
+                  {
+                    path: '/admin/classifyOrType',
+                    name: 'classifyOrType',
+                    icon: 'smile',
+                    component: './Welcome',
+                    authority: ['admin'],
+                  },
+                ],
+              },
+              {
+                path: '/knowledge',
+                name: 'knowledge',
+                icon: 'AlertOutlined',
+                component: './Admin',
+                authority: ['admin'],
+                routes: [
+                  {
+                    path: '/admin/add_knowledge',
+                    name: 'add_knowledge',
+                    icon: 'smile',
+                    component: './Welcome',
+                    authority: ['admin'],
+                  },
+                  {
+                    path: '/admin/query_knowledge',
+                    name: 'query_knowledge',
+                    icon: 'smile',
+                    component: './Welcome',
+                    authority: ['admin'],
+                  },
+
+                ],
+              },
+              {
+                path: '/tag',
+                name: 'tag',
+                icon: 'DatabaseOutlined',
+                component: './UserAnalyse',
+              },
+              {
+                path: '/company',
+                name: 'company',
+                icon: 'UsergroupAddOutlined',
+                component: './UserAnalyse',
+              },
+              {
+                path: '/feedback',
+                name: 'feedback',
+                icon: 'SolutionOutlined',
+                component: './UserAnalyse',
+              },
+              {
+                path: '/userInterview',
+                name: 'userInterview',
+                icon: 'smile',
+                component: './UserAnalyse',
+              },
+              {
+                component: './404',
+              },
+            ],
+          },
+          {
+            component: './404',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+];
---
+++ src/pages/User/login/index.tsx
@@ -0,0 +1,171 @@
+import {
+  AlipayCircleOutlined,
+  LockOutlined,
+  MailOutlined,
+  MobileOutlined,
+  TaobaoCircleOutlined,
+  UserOutlined,
+  WeiboCircleOutlined,
+} from '@ant-design/icons';
+import { Alert, Space, message, Tabs } from 'antd';
+import React, { useState } from 'react';
+import ProForm, { ProFormCaptcha, ProFormCheckbox, ProFormText } from '@ant-design/pro-form';
+import { useIntl, connect, FormattedMessage } from 'umi';
+// import { getFakeCaptcha } from '@/services/login';
+import type { Dispatch } from 'umi';
+import type { StateType } from '@/models/login';
+import type { LoginParamsType } from '@/services/login';
+import type { ConnectState } from '@/models/connect';
+
+import styles from './index.less';
+
+export type LoginProps = {
+  dispatch: Dispatch;
+  userLogin: StateType;
+  submitting?: boolean;
+};
+
+const LoginMessage: React.FC<{
+  content: string;
+}> = ({ content }) => (
+  <Alert
+    style={{
+      marginBottom: 24,
+    }}
+    message={content}
+    type="error"
+    showIcon
+  />
+);
+
+const Login: React.FC<LoginProps> = (props) => {
+  const { userLogin = {}, submitting } = props;
+  const { status, type: loginType } = userLogin;
+  const [type, setType] = useState<string>('account');
+  const intl = useIntl();
+
+  const handleSubmit = (values: LoginParamsType) => {
+    const { dispatch } = props;
+    dispatch({
+      type: 'login/login',
+      payload: { ...values, type },
+    });
+  };
+  return (
+    <div className={styles.main}>
+      <ProForm
+        initialValues={{
+          autoLogin: true,
+        }}
+        submitter={{
+          render: (_: any, dom: any[]) => dom.pop(),
+          submitButtonProps: {
+            loading: submitting,
+            size: 'large',
+            style: {
+              width: '100%',
+            },
+          },
+        }}
+        onFinish={(values: LoginParamsType) => {
+          handleSubmit(values as LoginParamsType);
+          return Promise.resolve();
+        }}
+      >
+        {/* <Tabs activeKey={type} onChange={setType}>
+          <Tabs.TabPane
+            key="account"
+            tab={intl.formatMessage({
+              id: 'pages.login.accountLogin.tab',
+              defaultMessage: '账户密码登录',
+            })}
+          />
+          <Tabs.TabPane
+            key="mobile"
+            tab={intl.formatMessage({
+              id: 'pages.login.phoneLogin.tab',
+              defaultMessage: '手机号登录',
+            })}
+          />
+        </Tabs> */}
+
+        {status === 'error' && loginType === 'account' && !submitting && (
+          <LoginMessage
+            content={intl.formatMessage({
+              id: 'pages.login.accountLogin.errorMessage',
+              defaultMessage: '账户或密码错误（admin/ant.design)',
+            })}
+          />
+        )}
+        {type === 'account' && (
+          <>
+            <ProFormText
+              name="account"
+              fieldProps={{
+                size: 'large',
+                prefix: <UserOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.username.placeholder',
+                defaultMessage: '用户名: admin or user',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.username.required"
+                      defaultMessage="请输入用户名!"
+                    />
+                  ),
+                },
+              ]}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: 'large',
+                prefix: <LockOutlined className={styles.prefixIcon} />,
+              }}
+              placeholder={intl.formatMessage({
+                id: 'pages.login.password.placeholder',
+                defaultMessage: '密码: huang123456',
+              })}
+              rules={[
+                {
+                  required: true,
+                  message: (
+                    <FormattedMessage
+                      id="pages.login.password.required"
+                      defaultMessage="请输入密码！"
+                    />
+                  ),
+                },
+              ]}
+            />
+          </>
+        )}
+
+        {status === 'error' && loginType === 'mobile' && !submitting && (
+          <LoginMessage content="验证码错误" />
+        )}
+        <div
+          style={{
+            marginBottom: 24,
+          }}
+        >
+          <ProFormCheckbox noStyle name="autoLogin">
+            <FormattedMessage id="pages.login.rememberMe" defaultMessage="自动登录" />
+          </ProFormCheckbox>
+
+        </div>
+      </ProForm>
+
+    </div>
+  );
+};
+
+export default connect(({ login, loading }: ConnectState) => ({
+  userLogin: login,
+  submitting: loading.effects['login/login'],
+}))(Login);
